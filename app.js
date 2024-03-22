74dc3097f99acb17973db7d9a67df4f212fde5b5
Dan Braun <danbraun@users.noreply.github.com>
2024-03-22 07:16:44 -0500

    with transitions

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const dialog = document.querySelector("dialog")
+const dialogs = document.querySelectorAll("dialog")
 const openModalButtons = document.querySelectorAll(".showModal")
 const allModalCards = document.querySelectorAll("dialog .card")
 let numOfCards = 0
 let currentCard = 1
+let previousCard = null
 
 openModalButtons.forEach( (button) => {
   numOfCards++
   button.addEventListener("click", (e) => {
     const openID = e.target.dataset.open
-    const modalCardVisible = dialog.querySelector(`[data-card="${openID}"]`)
-    modalCardVisible.setAttribute("aria-hidden", false);
     currentCard = openID
-    dialog.showModal()
+    setModalCard()
   })
 })
 
@@ -21,21 +20,35 @@ const prevButtons = document.querySelectorAll('.prev')
 
 nextButtons.forEach( (button) => {
   button.addEventListener('click', () => {
+    previousCard = currentCard
     currentCard = (currentCard < numOfCards) ? +currentCard+1 : 1
     setModalCard()
   })
 })
 prevButtons.forEach( (button) => {
   button.addEventListener('click', () => {
+    previousCard = currentCard
     currentCard = (currentCard > 1) ? +currentCard-1 : numOfCards
     setModalCard()
   })
 })
 
 const setModalCard = function() {
-  allModalCards.forEach((card) => {
-    card.setAttribute("aria-hidden", true)
-  })
-  const modalCardVisible = dialog.querySelector(`[data-card="${currentCard}"]`)
-  modalCardVisible.setAttribute("aria-hidden", false);
+  if (previousCard) document.querySelector(`#dialogID-${previousCard}`)
+    .classList.remove("show")
+  const modalCardVisible = document.querySelector(`#dialogID-${currentCard}`)
+  modalCardVisible.showModal()
+  modalCardVisible.classList.add("show")
 }
+
+dialogs.forEach((dialog) => {
+  const content = dialog.querySelector('.card-content')
+
+  content.addEventListener("transitionend", (e) => {
+    const style = window.getComputedStyle(e.target)
+    if (style.opacity === "0") {
+      console.log("close:", dialog)
+      dialog.close()
+    }
+  })
+})
